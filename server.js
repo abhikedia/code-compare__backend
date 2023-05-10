c4f65125205bc31dee7a49778abe03ae26ce164b
abhikedia <iamabhikedia74@gmail.com>
2023-05-10 15:48:34 +0530

    update port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1/code", codeRouter);
 
-const port = process.env.PORT;
+const port = 4000;
 
 app.listen(port, async () => {
   startContainer()
